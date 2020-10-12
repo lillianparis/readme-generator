71ac2e71641772dc80d1f6d55a3a8909ad378868
Lillian Paris <lillian.paris529@gmail.com>
2020-10-11 20:43:10 -0700

    Working on adding into the readme js file

--- utils/README.md
+++ utils/README.md
@@ -1,1 +1,1 @@
-Readme Readme Generator 
+# Readme
--- utils/makeReadMeFile.js
+++ utils/makeReadMeFile.js
@@ -4,18 +4,32 @@ function showLicenses (license){
     //display something like this project is using the ____ license
     if (license !== "None") {
         //getting the choices off of object array
-
-
-        return 
-        
-    }
-
+        return `![Github license](https://img.shields.io/badge/liscence-${license}-blue.svg)`
+        }
+        return ``
 } 
+function showLicenseLink(lisence) {
+    if (license !== "None") {
+        return (
+            `\n* [license](#license\n)`
+        )
+    }
+    return ``
+}
+function showLicenseSection(license) {
+    if (license !== "None") {
+        return (`## License
+        This project is licensed under the ${license} license`)
+    }
+    return``
+}
 
 
 
 function makeReadMeFile(data) {
-return `${data.Title} ${data.Description} `
+return `# ${data.Title}`
+// $(showLicenses(data.license))
+
 
 }
 
